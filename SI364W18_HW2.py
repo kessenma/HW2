f63ab3c8b823c338d5787f1e3e692b3ce5a3c754
Kyle Essenmacher <kessen@umich.edu>
2018-01-28 17:44:00 -0500

    push

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -16,6 +16,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
 
+import json
+import requests
+
 #####################
 ##### APP SETUP #####
 #####################
@@ -43,6 +46,41 @@ def hello_world():
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
 
+@app.route('/artistform')
+def get_artist_form():
+	return(render_template('artistform.html'))
+
+@app.route('/artistinfo')
+def get_artist_links():
+    if request.method == 'GET':
+        result = request.args
+        temp = {}
+        temp['term'] = result.get('artist')
+        resp = requests.get('https://itunes.apple.com/search?', params = temp)
+        data = json.loads(resp.text)
+        return render_template('artist_info.html',objects = data['results'])
+
+'''
+@app.route('/artistlinks')
+def get_artist_links():
+	return(render_template('artist_links.html'))
+
+@app.route('/album_entry')
+def get_album_entry():
+	return(render_template('album_entry.html'))
+
+@app.route('/album_result')
+def get_album_result():
+	return(render_template('album_entry.html'))
+'''
+
+
+
+'''
+✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨
+✨✨✨✨✨✨✨✨✨ DO NOT DELETE BELOW THIS✨✨✨✨✨✨✨✨
+✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨
+'''
 
 if __name__ == '__main__':
     app.run(use_reloader=True,debug=True)
